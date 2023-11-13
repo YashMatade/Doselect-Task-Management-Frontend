cb77c3618f7b7d25d6ed2708d5ebef31009663a1
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2023-11-13 15:50:55 +0530

    Main Logic removed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,70 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { getAllTasks, createTask, updateTaskById, deleteTaskById } from '../api/taskApi';
+import React from 'react';
 import './TaskList.css';
 
 const TaskList = () => {
-    const [tasks, setTasks] = useState([]);
-    const [newTaskTitle, setNewTaskTitle] = useState('');
-    const [newTaskDescription, setNewTaskDescription] = useState('');
-    const [selectedTaskId, setSelectedTaskId] = useState(null);
-    const [editedTaskTitle, setEditedTaskTitle] = useState('');
-    const [editedTaskDescription, setEditedTaskDescription] = useState('');
 
-    useEffect(() => {
-        const fetchTasks = async () => {
-            const tasksData = await getAllTasks();
-            setTasks(tasksData);
-        };
-        fetchTasks();
-    }, []);
-
-    const handleCreateTask = async () => {
-        if (newTaskTitle && newTaskDescription) {
-            const newTask = { title: newTaskTitle, description: newTaskDescription };
-            const createdTask = await createTask(newTask);
-            setTasks((prevTasks) => [...prevTasks, createdTask]);
-            setNewTaskTitle('');
-            setNewTaskDescription('');
-        }
-    };
-
-    const handleUpdateTask = async (taskId, updatedTitle, updatedDescription) => {
-        const updatedTask = await updateTaskById(taskId, { title: updatedTitle, description: updatedDescription });
-        setTasks((prevTasks) =>
-            prevTasks.map((task) => (task._id === updatedTask._id ? updatedTask : task))
-        );
-        setSelectedTaskId(null);
-        setEditedTaskTitle('');
-        setEditedTaskDescription('');
-    };
-
-    const handleDeleteTask = async (taskId) => {
-        await deleteTaskById(taskId);
-        setTasks((prevTasks) => prevTasks.filter((task) => task._id !== taskId));
-    };
-
-    const handleEditTask = (taskId, title, description) => {
-        setSelectedTaskId(taskId);
-        setEditedTaskTitle(title);
-        setEditedTaskDescription(description);
-    };
+    // Write your code here to complete the fuctionality
 
     return (
         <div className="task-list-container">
             <div className="task-form">
                 <input
                     type="text"
-                    value={newTaskTitle}
-                    onChange={(e) => setNewTaskTitle(e.target.value)}
                     placeholder="Enter new task title"
                 />
                 <input
                     type="text"
-                    value={newTaskDescription}
-                    onChange={(e) => setNewTaskDescription(e.target.value)}
                     placeholder="Enter new task description"
                 />
-                <button className="btn-create" onClick={handleCreateTask}>
+                <button className="btn-create" >
                     Create Task
                 </button>
             </div>
@@ -78,36 +30,7 @@ const TaskList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {tasks.map((task) => (
-                        <tr key={String(task._id)}>
-                            <td>{task._id === selectedTaskId ? <input type="text" value={editedTaskTitle} onChange={(e) => setEditedTaskTitle(e.target.value)} /> : task.title}</td>
-                            <td>{task._id === selectedTaskId ? <input type="text" value={editedTaskDescription} onChange={(e) => setEditedTaskDescription(e.target.value)} /> : task.description}</td>
-                            <td>
-                                {task._id === selectedTaskId ? (
-                                    <>
-                                        <button
-                                            className="btn-update"
-                                            onClick={() => handleUpdateTask(task._id, editedTaskTitle, editedTaskDescription)}
-                                        >
-                                            Update
-                                        </button>
-                                        <button className="btn-delete" onClick={() => setSelectedTaskId(null)}>
-                                            Cancel
-                                        </button>
-                                    </>
-                                ) : (
-                                    <>
-                                        <button className="btn-update" onClick={() => handleEditTask(task._id, task.title, task.description)}>
-                                            Edit
-                                        </button>
-                                        <button className="btn-delete" onClick={() => handleDeleteTask(task._id)}>
-                                            Delete
-                                        </button>
-                                    </>
-                                )}
-                            </td>
-                        </tr>
-                    ))}
+                    {/* Write your code here */}
                 </tbody>
             </table>
         </div>
